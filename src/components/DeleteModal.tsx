cbf253f941ca475b8fc5ca5f561d5a3a78ea5bad
Eric Winkelspecht <105669475+ericwink@users.noreply.github.com>
2023-01-02 13:32:44 -0500

    Revert "Revert "Edit delete board""

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -5,19 +5,24 @@ type Props = {
   setDeleteModal: React.Dispatch<React.SetStateAction<boolean>>;
   item: string;
   title: string | undefined;
-  removeTask: () => void;
+  removeItem: () => void;
 };
 
-export default function DeleteModal({ setDeleteModal, item, title, removeTask }: Props) {
+export default function DeleteModal({ setDeleteModal, item, title, removeItem }: Props) {
   const theme = useContext(ThemeContext);
+
+  const deleteAndClose = () => {
+    removeItem();
+    setDeleteModal(false);
+  };
 
   return (
     <>
       <div id="modal-background" onClick={() => setDeleteModal(false)}></div>
-      <div id="delete-modal" className={`modal ${theme}`}>
+      <div id="delete-modal" className={`modal ${theme?.theme}`}>
         <h1 className="heading-l">{`Delete this ${item}?`}</h1>
         <p className="body-m">{`Are you sure you want to delete the '${title}' ${item}? This action will remove all ${item === "task" ? "task data and subtasks" : "columns and tasks"} and cannot be reversed`}</p>
-        <button className="btn-l btn-destructive" onClick={removeTask}>
+        <button className="btn-l btn-destructive" onClick={deleteAndClose}>
           Delete
         </button>
         <button className="btn-l btn-secondary" onClick={() => setDeleteModal(false)}>
